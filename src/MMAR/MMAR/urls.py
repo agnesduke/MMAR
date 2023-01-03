d5de0d32d77a0bc7567c379da27287ab4a50e07a
agnesduke <www.agnesduke@gmail.com>
2023-01-03 16:11:40 +0100

    linked pages

--- src/MMAR/MMAR/urls.py
+++ src/MMAR/MMAR/urls.py
@@ -23,3 +23,4 @@ urlpatterns = [
 ]
 urlpatterns += path('', include('core.urls')),
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATICFILES_DIRS)
--- src/MMAR/core/views.py
+++ src/MMAR/core/views.py
@@ -5,7 +5,43 @@ from .models import Client, Service, Category
 
 
 class Home(View):
-    template_name = None
+    template_name = 'societe.html'
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,self.template_name)
+
+  
+class ClientPage(View):
+    template_name = 'client.html'
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,self.template_name)
+
+    def post(self,request,*args,**kwargs):
+        return render(request,self.template_name)    
+
+class ServicesPage(View):
+    template_name = 'services.html'
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,self.template_name)
+
+    def post(self,request,*args,**kwargs):
+        return render(request,self.template_name)    
+
+class comptePage(View):
+    template_name = 'compte.html'
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,self.template_name)
+
+    def post(self,request,*args,**kwargs):
+        return render(request,self.template_name)    
+
 
 
 # Create your views here.
